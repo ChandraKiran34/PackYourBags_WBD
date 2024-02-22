e336318b506bf65c2e4d93ca3afe941ecd0b94a1
RNutro <97390378+RN1015@users.noreply.github.com>
2024-02-22 23:48:28 +0530

    Delete back-end directory

--- back-end/routes/TripRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createTrip } from '../controllers/tripController.js';
-
-const router = express.Router();
-
-// Route to create a new trip
-router.post('/', createTrip);
-
-export default router;
